04b9889b9fe17d9602290d52ae03eb466ebfd060
pawanbhul <bhulpawan4@gmail.com>
2026-03-14 21:02:37 +0545

    updated

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+"""
+model: python class used to represent a database table
+
+syntax:
+class Model_name(models.Model):
+    feild.......
+
+"""
+class Student(models.Model):
+    name=models.CharField(max_length=100,null=True,blank=True)
+    age=models.IntegerField(default=18)
+    marks=models.IntegerField()
+    city=models.CharField(max_length=50)
+    email=models.CharField(max_length=100,unique=True)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+
--- app/views.py
+++ app/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
+
+# select * from Student
 def index(request):
-    students=[
-            {"name": "pawan","age":23,"marks":80},
-            {"name": "pooja","age":23,"marks":99},
-            {"name": "sapana","age":21,"marks":77},
-            {"name": "santosh","age":22,"marks":47},
-        ]
+    students= Student.objects.all()
     context={"students":students}
         
             
